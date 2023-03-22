cc8b0c1657450163d29c8a3152b0bdef6fcd244f
DagKienefi <kien.dang@efisoft.com.vn>
2023-03-22 18:32:43 +0700

    demo connect wallet and transfer

---
+++ cardano-app/cardano-app/src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MeshProvider } from "@meshsdk/react";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <MeshProvider>
+      <Component {...pageProps} />
+    </MeshProvider>
+  );
+}
+
+export default App;
