1de3a1a4a9a048c708ec2c0390bf9a7c69691532
phandang <phdang94@yahoo.com>
2023-03-23 07:58:46 +0700

    edit: transfer token

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,110 @@
-import { AssetMetadata, ForgeScript, Mint, Transaction } from "@meshsdk/core";
+import { AssetMetadata, ForgeScript, largestFirst, Mint, Transaction } from "@meshsdk/core";
 import { CardanoWallet, MeshBadge, useWallet } from "@meshsdk/react";
 import { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const TADA_LOVELACE = 1000000
+const SC_ADDRESS = 'addr_test1wzq2s9tr9a0m2skv3h0unp6k38ttcmnprkvlv0xsksdcgfc8py8kw'
+
 
 const Home: NextPage = () => {
   const { connected, wallet } = useWallet();
   const [assets, setAssets] = useState<null | any>(null);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [lockedTADA, setLockedTADA] = useState<string>('');
+  // const [loading, setLoading] = useState<boolean>(false);
   const [address, setAddress] = useState("");
   const [transferTo, setTransferTo] = useState({ address: null, amount: null });
 
-  async function getAssets() {
-    if (connected) {
-      setLoading(true);
-      const _assets = await wallet.getAssets();
-      setAssets(_assets);
-      setLoading(false);
-      const address_wallet = await wallet?.getChangeAddress();
-      setAddress(address_wallet);
+  // async function getAssets() {
+  //   if (connected) {
+  //     setLoading(true);
+  //     const _assets = await wallet.getAssets();
+  //     setAssets(_assets);
+  //     setLoading(false);
+  //     const address_wallet = await wallet?.getChangeAddress();
+  //     setAddress(address_wallet);
+  //   }
+  // }
+
+  useEffect(() => {
+    const getAddress = async () => {
+      if (!connected) return
+      const addressWallet = await wallet?.getChangeAddress();
+      setAddress(addressWallet);
+    }
+    getAddress()
+  }, [connected])
+
+  async function lockToken() {
+    if (!address || !lockedTADA) {
+      alert("Please enter amount to lock TADA...");
+      return;
+    }
+    if (+lockedTADA < 1) {
+      alert("Lock TADA must be greater than or equal 1 TADA");
+      return;
+    }
+    const utxos = await wallet.getUtxos();
+    const costLovelace = `${+lockedTADA * TADA_LOVELACE}`
+    const selectedUtxos = largestFirst(costLovelace, utxos, true);
+    const d = {
+      alternative: 0,
+      fields: [42],
+    };
+    const tx = new Transaction({ initiator: wallet })
+    tx.setChangeAddress(address)
+    tx.setTxInputs(selectedUtxos)
+    tx.sendAssets({
+      address: SC_ADDRESS,
+      datum: {
+        value: d,
+      },
+    },
+    [
+    ],).sendLovelace(SC_ADDRESS, costLovelace)
+    const unsignedTx = await tx.build();
+    const signedTx = await wallet.signTx(unsignedTx);
+    const txHash = await wallet.submitTx(signedTx);
+    if (txHash) {
+      setLockedTADA('')
+      alert(`TADA locked successfully!\n#${txHash}`)
     }
   }
-  async function SendADA() {
+
+  async function sendADA() {
     const { address, amount } = transferTo;
     if (!address || !amount) {
       alert("Please enter Address and amount...");
       return;
     }
-    if (amount < 1000000) {
-      alert("Amount must be than or equal 1000000 lovelace");
+    if (amount < 1) {
+      alert("Amount must be greater than or equal 1 TADA");
       return;
     }
+    const lovelace = amount * TADA_LOVELACE
     const tx = new Transaction({ initiator: wallet }).sendLovelace(
       address,
-      amount
+      `${lovelace}`
     );
     const unsignedTx = await tx.build();
     const signedTx = await wallet.signTx(unsignedTx);
     const txHash = await wallet.submitTx(signedTx);
+    if (txHash) {
+      alert(`TADA sent successfully!\n#${txHash}`)
+    }
   }
 
-  const handleChange = (e: any) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTransferTo({
       ...transferTo,
       [e.target.name]: e.target.value,
     });
   };
+
+  const handleLockChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLockedTADA(e.target.value)
+  };
+  
 
   // async function mintNFT() {
   //   const forgingScript = ForgeScript.withOneSignature(address);
@@ -86,9 +146,8 @@ const Home: NextPage = () => {
           gap: "50px",
         }}
       >
-        <MeshBadge dark={true} />
         {connected && address}
-        <CardanoWallet />
+        <CardanoWallet label="Connect to wallet"/>
         {connected && assets && (
           <>
             <h3>NFT Listing</h3>
@@ -124,8 +183,17 @@ const Home: NextPage = () => {
                 </div>
               ))}
             </div>
-            <div>
-              <div
+            
+            {/* <div>
+              <button onClick={() => mintNFT()} style={{ padding: "20px" }}>
+                Mint NFT
+              </button>
+            </div> */}
+          </>
+        )}
+        {connected && (
+        <>
+        <div
                 style={{
                   display: "flex",
                   justifyContent: "center",
@@ -155,7 +223,7 @@ const Home: NextPage = () => {
                   />
                   <input
                     name="amount"
-                    placeholder="amount"
+                    placeholder="TADA amount"
                     onChange={handleChange}
                     style={{
                       padding: "10px",
@@ -164,26 +232,21 @@ const Home: NextPage = () => {
                     }}
                   />
                   <button
-                    onClick={SendADA}
+                    onClick={sendADA}
                     style={{ padding: "10px", borderRadius: "10px" }}
                   >
                     Transfer
                   </button>
                 </div>
               </div>
-            </div>
-            {/* <div>
-              <button onClick={() => mintNFT()} style={{ padding: "20px" }}>
-                Mint NFT
-              </button>
-            </div> */}
-          </>
+        </>
         )}
+              
       </div>
 
-      <button onClick={() => getAssets()} style={{ padding: "20px" }}>
+      {/* <button onClick={() => getAssets()} style={{ padding: "20px" }}>
         Get Assets
-      </button>
+      </button> */}
     </>
   );
 };
